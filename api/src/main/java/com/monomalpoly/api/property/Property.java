444167b9af387eeddc62e414605e414be9e538f1
DELHAYE Thomas <thomas.del.haye@outlook.fr>
2019-02-06 14:54:58 +0100

    Property improve and downgrade

--- api/src/main/java/com/monomalpoly/api/property/Property.java
+++ api/src/main/java/com/monomalpoly/api/property/Property.java
@@ -11,31 +11,34 @@ import com.monomalpoly.api.player.Player;
 @Entity
 public class Property {
 
+	public static final int HOUSEPRICE = 100;
+	public static final int HOSTELPRICE = 300;
+
     @Id
     @GeneratedValue
     private int id;
     private String name;
-    private String nature;
+    private String nature; // normal, special, etc
     private boolean buyable;
     @OneToOne
     private Player user;
-    private int rent;
+    private int landCost; // prix terrain
+    private int cost;  // prix terrain + aménagement
+    private int level; // 0 = pas acheter 1 = terrain, 2 = maison, 3 = hotel
 
     public Property() {
 
     }
 
-	public Property(String name, String nature, boolean buyable, Player user, int rent) {
+	public Property(String name, String nature, boolean buyable, Player user, int landCost) {
 		super();
 		this.name = name;
 		this.nature = nature;
 		this.buyable = buyable;
 		this.user = user;
-		this.rent = rent;
-	}
-
-	public int getId() {
-		return id;
+		this.landCost = landCost;
+		this.cost = landCost;
+		this.level = 0;
 	}
 
 	public String getName() {
@@ -54,7 +57,49 @@ public class Property {
 		return user;
 	}
 
-	public int getRent() {
-		return rent;
+	public int getLandCost() {
+		return landCost;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setNature(String nature) {
+		this.nature = nature;
+	}
+
+	public void setBuyable(boolean buyable) {
+		this.buyable = buyable;
+	}
+
+	public void setUser(Player user) {
+		this.user = user;
+	}
+
+	public void setLandCost(int landCost) {
+		this.landCost = landCost;
+	}
+
+	public void improve(Player player) {
+		if (this.level < 2) {
+			switch(this.level) {
+			case 1: this.level++; this.cost = (int) (this.landCost * 0.1); player.removeToBalance(HOUSEPRICE); // augmentation de 10% du prix du terrain si maison (level 1)
+			break;
+			case 2: this.level++; this.cost = (int) (this.landCost * 0.3); player.removeToBalance(HOSTELPRICE);// augmentation de 30% du prix du terrain si hotel (level 2)
+			break;			
+			}
+		}
+	}
+	
+	public void downgrade(Player player){
+		if (this.level > 0) {
+			switch(this.level) {
+			case 3: this.level--; this.cost = (int) (this.landCost * 0.1); player.addToBalance(HOSTELPRICE);//passage au level 2
+			break;
+			case 2: this.level--; this.cost = (int) (this.landCost); player.addToBalance(HOUSEPRICE);// passage au level 1
+			break;			
+			}
+		}
 	}
 }
