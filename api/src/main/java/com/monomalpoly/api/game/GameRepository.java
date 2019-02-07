b20201eb726ab7686ab128b5db959b2dca48a63c
Robin Dupret <robin.dupret@gmail.com>
2019-02-07 17:58:47 +0100

    Fix the game controller
    Properly return a single game all the time and trigger the last
    one as we can't deterministically know the ID attached to the
    current game.

--- api/src/main/java/com/monomalpoly/api/game/GameRepository.java
+++ api/src/main/java/com/monomalpoly/api/game/GameRepository.java
@@ -1,7 +1,15 @@
 package com.monomalpoly.api.game;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
 
 public interface GameRepository extends CrudRepository<Game, Long> {
-	public Game findById(int id);
+	@Query(value = "SELECT game FROM Game game")
+	List<Game> findFirst(Pageable limit);
+
+	Game findById(int id);
 }
--- api/src/main/java/com/monomalpoly/api/game/GameRestController.java
+++ api/src/main/java/com/monomalpoly/api/game/GameRestController.java
@@ -4,8 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import org.springframework.data.domain.PageRequest;
+
 import java.util.List;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 
 @RestController
 public class GameRestController {
@@ -24,18 +29,25 @@ public class GameRestController {
     }
 
     @RequestMapping("game/countNbUsers")
-    public int getCountNbUsers() {
-        Game g = gameRepository.findById(1);
+    public HashMap<String, Integer> getCountNbUsers() {
+        HashMap<String, Integer> response = new HashMap<String, Integer>();
 
-        return g.getCountNbUsers();
+        response.put("value", getLastGame().getCountNbUsers());
+
+        return response;
     }
 
     @RequestMapping("game/decreaseCountNbUsers")
-    public int decreaseCountNbUsers() {
-        Game g = gameRepository.findById(1);
+    public HashMap<String, Integer> decreaseCountNbUsers() {
+        Game g = getLastGame();
         g.setCountNbUsers(g.getCountNbUsers() - 1);
         gameRepository.save(g);
-        return g.getCountNbUsers();
+
+        HashMap<String, Integer> response = new HashMap<String, Integer>();
+
+        response.put("value", g.getCountNbUsers());
+
+        return response;
     }
 
     @RequestMapping("game/editState/{state}")
@@ -47,17 +59,23 @@ public class GameRestController {
     }
 
     @RequestMapping("game")
-    public List<Game> games() {
-        List<Game> l = new ArrayList<Game>();
-        for(Game g : gameRepository.findAll())
-            l.add(g);
-        if(l.isEmpty()) {
+    public Game games() {
+        Iterator<Game> games = gameRepository.findAll().iterator();
+
+        if (games.hasNext()) {
+            return games.next();
+        } else {
             Game g = new Game();
             g.setNbUsers(-1);
             g.setCountNbUsers(-1);
             g.setState("null");
-            l.add(g)
+
+            return g;
         }
-        return l;
+    }
+
+    private Game getLastGame() {
+        List<Game> games = gameRepository.findFirst(new PageRequest(0, 1));
+        return games.get(0);
     }
 }
