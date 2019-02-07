a63013c2b69c1d8de2c39f9ae449acbf049dbd94
Samuel Bazniar <tehsams@gmail.com>
2019-02-07 15:18:56 +0100

    Tri des import dans l'API

--- api/src/main/java/com/monomalpoly/api/player/PlayerRepository.java
+++ api/src/main/java/com/monomalpoly/api/player/PlayerRepository.java
@@ -1,8 +1,6 @@
 package com.monomalpoly.api.player;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.jpa.repository.Query;
-import java.util.ArrayList;
 
 public interface PlayerRepository extends CrudRepository<Player, Long> {
 
