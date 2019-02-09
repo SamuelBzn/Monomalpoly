c158e04d29bc248846a1618665653d8ea277ca32
Robin Dupret <robin.dupret@gmail.com>
2019-02-09 18:52:41 +0100

    Use `int` instead of `long` for dices
    There is no need to store a `long` number here as we are drawing
    values between 1 and 6.

--- api/src/main/java/com/monomalpoly/api/dice/Dice.java
+++ api/src/main/java/com/monomalpoly/api/dice/Dice.java
@@ -3,16 +3,16 @@ package com.monomalpoly.api.dice;
 public class Dice {
 
     private final boolean isDouble;
-    private final long value;
+    private final int value;
     private final String message;
 
-    public Dice(long value, boolean isDouble, String message) {
+    public Dice(int value, boolean isDouble, String message) {
         this.value = value;
         this.isDouble = isDouble;
         this.message = message;
     }
 
-    public long getValue() {
+    public int getValue() {
         return value;
     }
 
