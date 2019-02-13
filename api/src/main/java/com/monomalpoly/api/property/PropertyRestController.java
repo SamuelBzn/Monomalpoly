17dae531de1203c2c00fddbcac7de9cb76992b25
Robin Dupret <robin.dupret@gmail.com>
2019-02-13 13:27:20 +0100

    Add the routes to buy properties and houses
    Also add a route to return a message in case the user refuses to
    buy the thing that they can buy during the game.

--- api/src/main/java/com/monomalpoly/api/property/PropertyRestController.java
+++ api/src/main/java/com/monomalpoly/api/property/PropertyRestController.java
@@ -6,10 +6,29 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.ArrayList;
 
+import com.monomalpoly.api.card.Card;
+import com.monomalpoly.api.game.Game;
+import com.monomalpoly.api.game.GameRepository;
+import com.monomalpoly.api.property.Property;
+import com.monomalpoly.api.property.PropertyRepository;
+import com.monomalpoly.api.player.PlayerRepository;
+import com.monomalpoly.api.player.Player;
+
+import com.monomalpoly.api.BaseController;
+import com.monomalpoly.api.JSONResponse;
+
+import java.util.HashMap;
+
 @RestController
-public class PropertyRestController {
+public class PropertyRestController extends BaseController {
+    @Autowired
+    private GameRepository gameRepository;
+
     @Autowired
     private PropertyRepository propertyRepository;
+
+    @Autowired
+    private PlayerRepository playerRepository;
 
     @RequestMapping("property")
     public List<Property> properties() {
@@ -17,5 +36,47 @@ public class PropertyRestController {
         for(Property p : propertyRepository.findAll())
             l.add(p);
         return l;
+    }
+
+    @RequestMapping("/buy/property")
+    public HashMap<String, Object> buyProperty() {
+        Game game = getLastGame();
+        Player current = game.getCurrentPlayer();
+        List<Card> cards = game.getBoard().getCards();
+
+        Property property = (Property)cards.get(current.getPosition());
+
+        property.setUser(current);
+        propertyRepository.save(property);
+
+        current.removeToBalance(property.getLandCost());
+        playerRepository.save(current);
+
+        return JSONResponse.builder()
+            .with("message", "Vous avez acheté cette propriété.")
+            .build();
+    }
+
+    @RequestMapping("/buy/property/house")
+    public HashMap<String, Object> buyHouse() {
+        Game game = getLastGame();
+        Player current = game.getCurrentPlayer();
+        List<Card> cards = game.getBoard().getCards();
+
+        Property property = (Property)cards.get(current.getPosition());
+
+        property.addToHouses(1);
+        propertyRepository.save(property);
+
+        return JSONResponse.builder()
+            .with("message", "Vous avez acheté une maison pour cette propriété.")
+            .build();
+    }
+
+    @RequestMapping("/buy/refuse")
+    public HashMap<String, Object> refuse() {
+        return JSONResponse.builder()
+            .with("message", "Vous n'avez rien acheté")
+            .build();
     }
 }
